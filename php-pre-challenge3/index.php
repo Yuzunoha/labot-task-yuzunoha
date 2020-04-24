179859834f4a5a3d2e1ddf15fd8ede3011073ca5
Yuzunoha <studysteadystandby@gmail.com>
2020-04-24 23:38:30 +0900

    dockerでやろう

---
+++ php-pre-challenge3/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * dockerでやろう
+ * 
+ * https://quelcode.slite.com/app/channels/O3DXWCnVGJ/notes/pN5W_6E_aO
+ */
+class phpPreChallenge3
+{
+  public static function run(array $a, int $target)
+  {
+  }
+}
