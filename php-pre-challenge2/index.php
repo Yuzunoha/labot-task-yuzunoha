6c796d6f9096a4b4269bbb9d7920616d5eac2b42
Yuzunoha <studysteadystandby@gmail.com>
2020-04-27 20:03:01 +0900

    バブルソート

--- php-pre-challenge2/index.php
+++ php-pre-challenge2/index.php
@@ -1,7 +1,25 @@
 <?php
-$array = [1, 2, 3];
-// 要素数回繰り返し
-for ($i = 0; $i < count($array); $i++) {
-  //ここからコーディング
-}
+$array = array(3, 2, 1, 4, 15, 18, 13, 99, 77, 66, 1, 100, 0);
+bubbleSort($array);
 print_r($array);
+
+function swap(array &$a, int $idxLeft, int $idxRight): void
+{
+  $tmp = $a[$idxLeft];
+  $a[$idxLeft] = $a[$idxRight];
+  $a[$idxRight] = $tmp;
+}
+
+function bubbleSort(array &$a): void
+{
+  $len = count($a);
+  for ($i = 0; $i < $len; $i++) {
+    for ($j = 0; $j < $len - $i - 1; $j++) {
+      $idxLeft = $j;
+      $idxRight = $j + 1;
+      if ($a[$idxRight] < $a[$idxLeft]) {
+        swap($a, $idxLeft, $idxRight);
+      }
+    }
+  }
+}
